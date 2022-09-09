ee68c1747e7f885231eee417179d830f0b76b13d
SandraChege <chegemwihaki88@gmail.com>
2022-09-09 08:05:09 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
-include <stdio.h>
-
-int main(void){
+include < stdio.h >
+/*
+ *prints exactly with proper grammar, but the outcome is a piece of art
+ *Conditions use print function
+ *		don't use function puts
+ *		return zero
+ *		compile without warning
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
