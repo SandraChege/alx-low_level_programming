8d6dac566604fdfe45e15c4b153de205146cc2a4
SandraChege <chegemwihaki88@gmail.com>
2022-09-09 12:59:30 -0700

    4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ * main-prints all alphabetic letters except q and e in lowecase
+ * return 0 success
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{ 
+		if(ch !='e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
