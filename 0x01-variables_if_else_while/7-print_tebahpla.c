932c0d630f57aa31ced7428ce6bda01c7b6308e6
SandraChege <chegemwihaki88@gmail.com>
2022-09-10 04:28:54 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function
+ *
+ * Description: prints a-z in reverse mode
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char letters = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
