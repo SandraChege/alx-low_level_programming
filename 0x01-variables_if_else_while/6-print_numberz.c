87dc79c2565f0c6c07f9dea77562e15aec34880c
SandraChege <chegemwihaki88@gmail.com>
2022-09-10 03:50:56 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,15 +9,14 @@
 
 int main(void)
 {
-	int num = 0;
+	int num;
 
-	while (num <= 9)
+	for (num = 0; num <= 9; num++)
 	{
-		putchar("%1", num);
-		num++;
+		putchar(num);
 	}
 	
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
