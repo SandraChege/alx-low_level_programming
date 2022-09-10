1498d75c90e289bd851fdd71fb8fee4351b65b55
SandraChege <chegemwihaki88@gmail.com>
2022-09-10 05:59:21 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@
 int main(void)
 {
 		int digit = 0;
-		
+
 		while (digit <= 9)
 		{
 			putchar(digit + 48);
-			
+
 			if (digit != 9)
 			{
 				putchar(',');
@@ -23,7 +23,7 @@ int main(void)
 
 			digit++;
 		}
-		
+
 		putchar('\n');
 
 		return (0);
