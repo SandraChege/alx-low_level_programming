cad6e9a904b4d92c228152fac64abb25f76f48d5
SandraChege <chegemwihaki88@gmail.com>
2022-09-10 02:15:25 -0700

    code has an error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - This is the main function
+ * Description- Prints: "and that piece of art is useful" - Dora Korpar,
+ * 2015-10-19.
+ *
+ * Return (1) -An error occurs
+*/
+
+int main(void)
+{
+	char quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, quote, sizeof(quote));
+
+	return (1);
+}
