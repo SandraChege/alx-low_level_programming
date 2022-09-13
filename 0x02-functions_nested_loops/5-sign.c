15e295768ed527ded44d7912ce1fc010c1be07d1
SandraChege <chegemwihaki88@gmail.com>
2022-09-13 12:10:39 -0700

    returns sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,22 +3,26 @@
 /**
  * print_sign - prints sign of a number
  *
- * Return : (1) if +, 0 if 0 and (-1) if -
+ * Return:(1) if +, 0 if 0 and (-1) if -
 */
 
 int print_sign(int n)
 {
+	int color;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		color = 1;
 	} else if (n == 0)
 	{
 		_putchar('0');
-		return (0);
+		color = 0;
 	} else
 	{
 		_putchar('-');
-		return (-1);
+		color = -1;
 	}
+
+	return (color);
 }
