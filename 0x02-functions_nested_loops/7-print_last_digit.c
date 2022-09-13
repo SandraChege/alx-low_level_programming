f23f052d57650dcb422f62236c2240f5fd0ef1b6
SandraChege <chegemwihaki88@gmail.com>
2022-09-13 14:02:21 -0700

    prints last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_last_digit: prints last digit of a number
- * @n: used as function argument
+ * print_last_digit - prints last digit of a number
+ * @n: number to ckeck
  *
  * Return: last digit
 */
