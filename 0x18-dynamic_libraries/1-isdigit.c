daba1122b5b1b010f62b258b7ab9b8f9015fc6fa
SandraChege <chegemwihaki88@gmail.com>
2022-12-13 12:30:02 -0800

    task 0 completed

--- 0x18-dynamic_libraries/1-isdigit.c
+++ 0x18-dynamic_libraries/1-isdigit.c
@@ -1,24 +1,22 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- *_strcat - concatenates  the string pointed to by @src to
- * the end of the string pointed to by @dest
- *@dest: String that will be appended
- *@src: String to be concatenated upon
+ * _isdigit - this is a function
+ * Description - Checks for a digit
+ * @c: function argument used
  *
- * Return: returns poiner to @dest
- */
+ * Return: (1) if digit and (0) otherwise
+*/
 
-char *_strcat(char *dest, char *src)
+int _isdigit(int c)
 {
-
-	int index = 0, dest_len = 0;
-
-	while (dest[index++])
-		dest_len++;
-
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
-
-	return (dest);
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
